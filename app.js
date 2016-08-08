b6b89004b8f803e59ba3b579fc111d468ee2d796
micah-eberhard <micah.eberhard@gmail.com>
2016-08-08 09:50:02 -0600

    Added more dynamic vision and a wider screen size

--- app.js
+++ app.js
@@ -34,6 +34,8 @@ function game(){
   *******/
   var BuildEntity = function(){
     return {
+      currentFlop : false,
+      ignoreFlop : false,
       location: {
         x: -1,
         y: -1
@@ -58,47 +60,51 @@ function game(){
       },
       actions: {
         move: function(dir){
-          console.log('Move!');
+          //console.log('Move!');
           char = this.Parent;
-          var newLoc = {};
-          if(dir === 'w')
+          if(world.currentFlop !== char.currentFlop || char.ignoreFlop)
           {
-            newLoc = {
-              x:char.location.x,
-              y:char.location.y-1
-            };
+            var newLoc = {};
+            if(dir === 'w')
+            {
+              newLoc = {
+                x:char.location.x,
+                y:char.location.y-1
+              };
+            }
+            else if(dir === 's')
+            {
+              newLoc = {
+                x:char.location.x,
+                y:char.location.y+1
+              };
+            }
+            else if(dir === 'a')
+            {
+              newLoc = {
+                x:char.location.x-1,
+                y:char.location.y
+              };
+            }
+            else if(dir === 'd')
+            {
+              newLoc = {
+                x:char.location.x+1,
+                y:char.location.y
+              };
+            }
+            if(moveValid(newLoc, world))
+            {
+              var hold = world.map[newLoc.x][newLoc.y].origin;
+              world.toOrigin(char.location);
+              world.map[newLoc.x][newLoc.y] = char;
+              world.map[newLoc.x][newLoc.y].origin = hold;
+              //console.log(world.map[newLoc.x]);
+              char.location = newLoc;
+            }
+            //console.log(char.location);
+            char.currentFlop = !char.currentFlop;
           }
-          else if(dir === 's')
-          {
-            newLoc = {
-              x:char.location.x,
-              y:char.location.y+1
-            };
-          }
-          else if(dir === 'a')
-          {
-            newLoc = {
-              x:char.location.x-1,
-              y:char.location.y
-            };
-          }
-          else if(dir === 'd')
-          {
-            newLoc = {
-              x:char.location.x+1,
-              y:char.location.y
-            };
-          }
-          if(moveValid(newLoc, world))
-          {
-            var hold = world.map[newLoc.x][newLoc.y].origin;
-            world.toOrigin(char.location);
-            world.map[newLoc.x][newLoc.y] = char;
-            world.map[newLoc.x][newLoc.y].origin = hold;
-            //console.log(world.map[newLoc.x]);
-            char.location = newLoc;
-          }
-          console.log(char.location);
         },
         jump: function(){
           console.log('Jump!');
@@ -151,7 +157,7 @@ function game(){
     hero.atr.intelligence = 8;
     hero.atr.charisma = 8;
     hero.content = 'x';
-    hero.atr.sight = 4;
+    hero.atr.sight = 0;
     hero.atr._sightOrigin = hero.atr.sight;
     return hero;
   };
@@ -159,11 +165,14 @@ function game(){
     b:boar,
     h:human
   };
+  var circleMap = ['x','3',3,'b','h'];
 
   var worldCreate = function(){
     var rowHold = [];
     return{
     map: [],
+    worldTag: arguments[0],
+    currentFlop: false,
     height: 500,
     width: 1000,
     scope: 10,
@@ -227,11 +236,19 @@ function game(){
         for(var j=0; j < this.height; j++)
         {
           this.map[i][j] = {};
+
           if((rowHold[j]) && (this.textureMap.types[rowHold[j].content] !== undefined || this.textureMap.types[this.map[i][j-1]] !== undefined)){
             this.map[i][j].content = this.randomItem(this.textureMap.types[rowHold[j].content]);
-          }else {
+          }
+          else {
             this.map[i][j].content = this.randomItem();
           }
+
+          if(j <= this.scope || i <= this.scope || i >= this.width - this.scope || j >= this.height - this.scope)
+          {
+            this.map[i][j].content = 1;
+          }
+
           this.map[i][j].origin = this.map[i][j].content;
           if(this.map[i][j].content !== 1)
             this.map[i][j] = this.randomEntity(this.map[i][j]);
@@ -292,12 +309,18 @@ function game(){
       } else {
         char = currThings;
       }
-      char.origin = currThings.origin;
+      if(currThings.content === '3' || currThings.content === 3)
+      {
+        char.origin = '0';
+      }
+      else{
+        char.origin = currThings.origin;
+      }
       return char;
     },
     getFadeLvl : function(i,j){
-      if((j-remi.location.x <= this.scope + remi.atr.sight)&&(i-remi.location.y <= this.scope + remi.atr.sight)){
-        fadeLvl = 0;
+      var fadeLvl = 0;
+      if((j-remi.location.x <= (this.scope*2) + remi.atr.sight)&&(i-remi.location.y <= (this.scope *2) + remi.atr.sight)){
         var ySet = i-remi.location.y;
         var xSet = j-remi.location.x;
         if(parseFloat(ySet) < 0)
@@ -308,25 +331,26 @@ function game(){
         if(xSet > ySet)
           fadeLvl = xSet;
         else
-          fadeLvl = ySet;
+          fadeLvl = ySet; //Set *2 for specular fade.
 
         fadeLvl = fadeLvl - remi.atr.sight;
       }
+
       return fadeLvl;
     },
     superCoverLine : function(p0, p1) {
-    var Point = function(newX,newY){
-      return{
-        x:newX,
-        y:newY
+      var Point = function(newX,newY){
+        return{
+          x:newX,
+          y:newY
+        };
       };
-    };
-    var dx = p1.x-p0.x, dy = p1.y-p0.y;
-    var nx = Math.abs(dx), ny = Math.abs(dy);
-    var sign_x = dx > 0? 1 : -1, sign_y = dy > 0? 1 : -1;
+      var dx = p1.x-p0.x, dy = p1.y-p0.y;
+      var nx = Math.abs(dx), ny = Math.abs(dy);
+      var sign_x = dx > 0? 1 : -1, sign_y = dy > 0? 1 : -1;
 
-    var p = new Point(p0.x, p0.y);
-    var points = [new Point(p.x, p.y)];
+      var p = new Point(p0.x, p0.y);
+      var points = [new Point(p.x, p.y)];
       for (var ix = 0, iy = 0; ix < nx || iy < ny;) {
         if ((0.5+ix) / nx == (0.5+iy) / ny) {
             // next step is diagonal
@@ -344,15 +368,18 @@ function game(){
             iy++;
         }
         points.push(new Point(p.x, p.y));
+
       }
-    return points;
+      //console.log(points);
+      return points;
     },
     display: function(){
       var dispStr = '';
+      var specialWorldTag = this.worldTag;
       for(var i=remi.location.y-this.scope; i < remi.location.y+this.scope; i++)
       {
         var col = '<div class="row r'+i+'">';
-        for(var j=remi.location.x-this.scope; j < remi.location.x+this.scope; j++)
+        for(var j=remi.location.x- (this.scope * 2); j < remi.location.x+ (this.scope *2); j++)
         {
           var currLoc = {
             x: j,
@@ -362,26 +389,119 @@ function game(){
           fadeLvl = this.getFadeLvl(i,j);
 
           var arrView = this.superCoverLine(remi.location, currLoc);
-          var view = true;
+
+          var secLoc = {x:remi.location.x, y:remi.location.y};
+          var secondaryView = [];
+
+          // X & Y Directions
+          secLoc.x ++;
+          if(this.map[secLoc.x][secLoc.y].content !== 1)
+            secondaryView.push(this.superCoverLine(secLoc, currLoc));
+
+          secLoc.x = secLoc.x-2;
+          if(this.map[secLoc.x][secLoc.y].content !== 1)
+            secondaryView.push(this.superCoverLine(secLoc, currLoc));
+
+          secLoc = {x:remi.location.x, y:remi.location.y};
+          secLoc.y ++;
+          if(this.map[secLoc.x][secLoc.y].content !== 1)
+            secondaryView.push(this.superCoverLine(secLoc, currLoc));
+
+          secLoc.y = secLoc.y -2;
+          if(this.map[secLoc.x][secLoc.y].content !== 1)
+            secondaryView.push(this.superCoverLine(secLoc, currLoc));
+
+          /**************/
+
+          // Diagonal Directions
+          secLoc = {x:remi.location.x, y:remi.location.y};
+          secLoc.y ++;
+          secLoc.x ++;
+          if(this.map[secLoc.x][secLoc.y].content !== 1)
+            secondaryView.push(this.superCoverLine(secLoc, currLoc));
+
+          secLoc = {x:remi.location.x, y:remi.location.y};
+          secLoc.y --;
+          secLoc.x --;
+          if(this.map[secLoc.x][secLoc.y].content !== 1)
+            secondaryView.push(this.superCoverLine(secLoc, currLoc));
+
+          secLoc = {x:remi.location.x, y:remi.location.y};
+          secLoc.y ++;
+          secLoc.x --;
+          if(this.map[secLoc.x][secLoc.y].content !== 1)
+            secondaryView.push(this.superCoverLine(secLoc, currLoc));
+
+          secLoc = {x:remi.location.x, y:remi.location.y};
+          secLoc.y --;
+          secLoc.x ++;
+          if(this.map[secLoc.x][secLoc.y].content !== 1)
+            secondaryView.push(this.superCoverLine(secLoc, currLoc));
+
+
+          /**************/
+
+          var view = false;
+          var directView = true;
+          var sideView = false;
           for(var k = 1; k < arrView.length-1; k++)
           {
             if(this.map[arrView[k].x][arrView[k].y].content === 1)
             {
-              view =  false;
+              directView =  false;
             }
           }
-          if(!view)
+          for(var v=0; v < secondaryView.length; v++)
           {
-            col = col + '<div class="grid loc'+i+','+j +' con' + this.map[j][i].content +' fade'+fadeLvl+' vHide">'+'</div>';
+            var chunkView = true;
+            for(var k = 1; k < secondaryView[v].length-1; k++)
+            {
+              if(this.map[secondaryView[v][k].x][secondaryView[v][k].y].content === 1)
+              {
+                chunkView =  false;
+              }
+            }
+            if(chunkView === true)
+            {
+                sideView = true;
+            }
           }
 
+          if(directView || sideView)
+          {
+            view = true;
+          }
+
+          var flag = false;
+
+
+          if(!view)
+          {
+            col = col + '<div class="grid loc'+i+','+j +' con' + specialWorldTag + this.map[j][i].content +' fade'+fadeLvl+' vHide">'+'</div>';
+          }
           else if(fadeLvl)
           {
-            col = col + '<div class="grid loc'+i+','+j +' con' + this.map[j][i].content +' fade'+fadeLvl+' vShow">'+'</div>';
-          }
-          else {
-            col = col + '<div class="grid loc'+i+','+j +' con' + this.map[j][i].content +' vShow">'+'</div>';
+            if(circleMap.indexOf(this.map[j][i].content) > -1)
+            {
+              flag = true;
+              col=col+'<div class ="charWrapper con' + specialWorldTag +  this.map[j][i].origin +' fade'+fadeLvl+'">';
             }
+            col = col + '<div class="grid loc'+i+','+j +' con' +  specialWorldTag+ this.map[j][i].content +' fade'+fadeLvl+' vShow">'+'</div>';
+          }
+          else
+          {
+            if(circleMap.indexOf(this.map[j][i].content) > -1)
+            {
+              flag = true;
+              col=col+'<div class ="charWrapper con' + specialWorldTag +  this.map[j][i].origin +' fade'+fadeLvl+'">';
+            }
+            col = col + '<div class="grid loc'+i+','+j +' con' + specialWorldTag + this.map[j][i].content +' vShow">'+'</div>';
+          }
+
+          if(flag)
+          {
+            col=col+'</div>';
+          }
         }
         col = col +'</div>';
         dispStr = dispStr + col;
@@ -404,6 +524,7 @@ function game(){
   };
   function runGame()
   {
+    world.currentFlop = !world.currentFlop;
     moveWorld(world);
     world.display();
   }
@@ -411,7 +532,7 @@ function game(){
   {
     var item = currWorld.map[loc.x][loc.y].content;
     var pass = false;
-    if(loc !== undefined && loc.x >= 0 && loc.y >=0 && loc.x <= currWorld.width && loc.y <=currWorld.height)
+    if(loc !== undefined && loc.x >= currWorld.scope && loc.y >= currWorld.scope && loc.x <= currWorld.width - currWorld.scope && loc.y <= currWorld.height - currWorld.scope)
     {
       try{
       var walk = currWorld.textureMap[currWorld.textureMap.types[item]].isWalkable;
@@ -445,15 +566,15 @@ function game(){
 
   function moveWorld(currWorld)
   {
-    for(var i=remi.location.x-world.scope; i < remi.location.x+world.scope; i++)
+    for(var i=remi.location.y-(currWorld.scope *2); i < remi.location.y+(currWorld.scope *2); i++)
     {
       var col = '<div class="row r'+i+'">';
-      for(var j=remi.location.y-world.scope; j < remi.location.y+world.scope; j++)
+      for(var j=remi.location.x-(currWorld.scope *2); j < remi.location.x+(currWorld.scope *2); j++)
       {
-        if(charMap[currWorld.map[i][j].content] !== undefined)
+        if(charMap[currWorld.map[j][i].content] !== undefined)
         {
           //var hold = currWorld.map[i][j].origin;
-          currWorld.map[i][j].actions.move(randomDir());
+          currWorld.map[j][i].actions.move(randomDir());
         }
       }
     }
@@ -526,14 +647,15 @@ Initial Display
 
 **********************/
 
-  var mainWorld = new worldCreate();
-  var belowWorld = new worldCreate();
+  var mainWorld = new worldCreate('');
+  var belowWorld = new worldCreate('U');
 
   var world = mainWorld;
 
   var remi = hero();
   remi.location.x = Math.round(world.width/2);
   remi.location.y = Math.round(world.height/2);
+  remi.ignoreFlop = true;
   console.log(remi);
   console.log(world);
 
@@ -543,50 +665,6 @@ Initial Display
   fillWorld(belowWorld);
   world.map[remi.location.x][remi.location.y].content = 'x';
   world.display();
-
-  belowWorld.display = function(){
-      var dispStr = '';
-      for(var i=remi.location.y-this.scope; i < remi.location.y+this.scope; i++)
-      {
-        var col = '<div class="row r'+i+'">';
-        for(var j=remi.location.x-this.scope; j < remi.location.x+this.scope; j++)
-        {
-          var currLoc = {
-            x: j,
-            y: i
-          };
-          var fadeLvl = false;
-          fadeLvl = this.getFadeLvl(i,j);
-
-          var arrView = this.superCoverLine(remi.location, currLoc);
-          var view = true;
-          for(var k = 1; k < arrView.length-1; k++)
-          {
-            if(this.map[arrView[k].x][arrView[k].y].content === 1)
-            {
-              view =  false;
-              //console.log("view: false");
-            }
-          }
-          if(!view)
-          {
-            col = col + '<div class="grid loc'+i+','+j +' conU' + this.map[j][i].content +' fade'+fadeLvl+' vHide">'+'</div>';
-          }
-
-          else if(fadeLvl)
-          {
-            col = col + '<div class="grid loc'+i+','+j +' conU' + this.map[j][i].content +' fade'+fadeLvl+' vShow">'+'</div>';
-          }
-          else {
-            col = col + '<div class="grid loc'+i+','+j +' conU' + this.map[j][i].content +' vShow">'+'</div>';
-            }
-        }
-        col = col +'</div>';
-        dispStr = dispStr + col;
-      }
-      worldPane.innerHTML = dispStr;
-      this.statsDisplay();
-    };
 
 
   btnInv.addEventListener('click', function(event){
